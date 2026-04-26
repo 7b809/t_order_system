9b963718f7c0d4e4158e9e15bea2018882787454
7b809 <ej818793@gmail.com>
2026-04-27 03:01:45 +0530

    Auto update

--- app.py
+++ app.py
@@ -335,4 +335,4 @@ def health():
 # -----------------------------------
 if __name__ == "__main__":
     logger.info("[INFO] Starting API")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
